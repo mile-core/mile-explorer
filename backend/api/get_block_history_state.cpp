7b27423f8042817fcd667a7633cc3b3e3974e64f
lotus mile <lotus@mile.global>
2018-11-26 14:15:03 +0300

    some block were missed fixed

--- backend/api/get_block_history_state.cpp
+++ backend/api/get_block_history_state.cpp
@@ -6,7 +6,7 @@
 
 static auto method = [](server::context &ctx, const ctxDb &db) {
     auto state = db->get_block_history_state();
-    ctx.response.result["count"] = state["block-id"];
+    ctx.response.result["count"] = state["block-id"].get<uint64_t>()+1;
     ctx.response.result["timestamp"] = state["timestamp"];
     ctx.response.result["first"] = 0;
 };
