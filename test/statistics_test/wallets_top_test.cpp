f32e952a7c0579cecb26fdff8cf3b9a3b6099f42
brzikofs <brzikofs@gmail.com>
2019-01-17 23:29:24 +0300

    finilize perfomance tests, separate new method from old into two files

--- test/statistics_test/wallets_top_test.cpp
+++ test/statistics_test/wallets_top_test.cpp
@@ -3,6 +3,8 @@
 
 #include "statistics.hpp"
 #include "milecsa.hpp"
+#include "../../indexer/statistics/wallets_top.cpp"
+#include "../../indexer/statistics/wallets_top_upd.cpp"
 
 #include <chrono>
 typedef std::chrono::high_resolution_clock Time;
@@ -11,12 +13,21 @@ typedef std::chrono::duration<float> fsec;
 
 BOOST_AUTO_TEST_CASE(wallets_top_test){
     auto db = milecsa::explorer::Db::Open();
-
     auto t0 = Time::now();
-    for(auto  &[name, method]: statistic::Registry::Instance().get_statistics()){
-        std::cout << name << endl;
-        std::cout << "WE R HERE" << endl;
-    }
+    method(db, 0);
+    auto t1 = Time::now();
+
+    fsec fs = t1 - t0;
+    ms d = std::chrono::duration_cast<ms>(fs);
+    std::cout << "TIME "<< std::endl;
+    std::cout << fs.count() << std::endl;
+    std::cout << d.count() << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(wallets_top_upd_test){
+    auto db = milecsa::explorer::Db::Open();
+    auto t0 = Time::now();
+    wallets_top_upd::method(db, 0);
     auto t1 = Time::now();
 
     fsec fs = t1 - t0;
